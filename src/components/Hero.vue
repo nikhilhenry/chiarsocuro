e903ef931d099706ef727e56c39b45c1da708b68
Nikhil Henry <nikhilhenry@Nikhils-MacBook-Pro.local>
2019-08-16 23:15:31 +0530

    create basic hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero">
+  <section class="hero is-fullheight-with-navbar">
+    <div class="hero-body">
+    <div class="container has-text-centered">
+      <p class="title">Chiaroscuro'19</p>
+      <p class="subtitle">September 20</p>
+    </div>
+    </div>
+  </section>
+  </div>  
+</template>
+
+<script>
+export default {
+  name:'Hero'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,15 +19,17 @@
   </div>
 </nav>
   </div>
+  <Hero/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Hero from '@/components/Hero'
 
 export default {
   name: 'home',
   components: {
+    Hero,
   },
   data(){
     return{
