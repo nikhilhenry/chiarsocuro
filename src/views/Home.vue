a52c9e7a25f2f2f44d6383beb6870e30b35266b2
Nikhil Henry <nikhilhenry@Nikhils-MacBook-Pro.local>
2019-08-16 23:07:05 +0530

    add navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <strong class="navbar-item">Chiaroscuro</strong>
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navItems" v-on:click="navToggle" v-bind:class="{'is-active':isActive}">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+  <div class="navbar-menu" id="navItems" v-bind="{'is-active':isActive}">
+    <div class="navbar-end">
+      <a class="navbar-item">Home</a>
+      <a class="navbar-item">Events</a>
+      <a class="navbar-item">Contact Us</a>
+    </div>
+  </div>
+</nav>
+  </div>
   </div>
 </template>
 
@@ -10,6 +28,27 @@
 export default {
   name: 'home',
   components: {
+  },
+  data(){
+    return{
+      isActive:false
+    }
+  },
+  methods:{
+    navToggle:function(){
+      this.isActive=!this.isActive;
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  nav{
+    .navbar-brand{
+      strong{
+        font-size: 1.5rem;
+        color: black;
+      }
+    }
+  }
+</style>
