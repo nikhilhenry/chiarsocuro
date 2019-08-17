e7c9285925e1ef30749e6daa287e8435e63fe1cd
Nikhil Henry <nikhilhenry@Nikhils-MBP.Dlink>
2019-08-17 18:17:42 +0530

    initilize firebase

---
+++ src/firbaseConfig.js
@@ -0,0 +1,9 @@
+module.exports ={
+  apiKey: "AIzaSyDYDz2VEC3NWsJI4uTQJyxLvepTC4kvJgM",
+  authDomain: "chiaroscuro19-e746d.firebaseapp.com",
+  databaseURL: "https://chiaroscuro19-e746d.firebaseio.com",
+  projectId: "chiaroscuro19-e746d",
+  storageBucket: "chiaroscuro19-e746d.appspot.com",
+  messagingSenderId: "825777964103",
+  appId: "1:825777964103:web:678ddd9e92aa9e2b"
+};
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueScrollTo from 'vue-scrollto'
+import firebase from 'firebase/app'
+import firebaseConfig from './firbaseConfig'
 
 Vue.config.productionTip = false
+
+// firebase intilization
+firebase.initializeApp(firebaseConfig);
 
 Vue.use(VueScrollTo, {
   container: "body",
