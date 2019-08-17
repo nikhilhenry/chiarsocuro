69fa14fcb4e6adf9529cea813771cfb65d2bf29e
Nikhil Henry <nikhilhenry@Nikhils-MBP.Dlink>
2019-08-17 09:14:30 +0530

    add lsb logo

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,6 +3,7 @@
   <section class="hero is-fullheight-with-navbar">
     <div class="hero-body">
     <div class="container has-text-centered">
+      <img src="../assets/legacy_logo.png" height="5px">
       <p class="title">Chiaroscuro'19</p>
       <p class="subtitle">September 20</p>
     </div>
@@ -16,3 +17,12 @@ export default {
   name:'Hero'
 }
 </script>
+
+<style lang="scss" scoped>
+.hero{
+  img{
+    max-height: 7rem;
+    margin-bottom: 6px;
+  }
+}
+</style>
