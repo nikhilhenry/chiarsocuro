50c59e9e22d254956e4af69f273b0151ca3d47e9
Nikhil Henry <nikhilhenry@Nikhils-MBP.Dlink>
2019-08-17 15:30:29 +0530

    complete form binding

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -8,26 +8,31 @@
     <div class="content">
       <p class="title">Registration</p>
       <p class="subtitle">{{event_title}}</p>
-      <form>
+      <!-- errors -->
+      <p v-if="errors.length">
+        <p v-bind:key="error" v-for="error in errors" class="has-background-danger has-text-white has-text-weight-semibold">{{error}}</p>
+      </p>
+      <form
+      @submit="submit">
         <!-- Full name -->
         <div class="field">
           <label class="label">Full Name</label>
           <div class="control">
-            <input class="input" type="text" placeholder="eg. David Stone">
+            <input class="input" type="text" placeholder="eg. David Stone" v-model="student_name">
           </div>
         </div>
         <!-- Student Email -->
         <div class="field">
           <label class="label">Student Email</label>
           <div class="control">
-            <input class="input" type="email" placeholder="eg. student@example.com">
+            <input class="input" type="email" placeholder="eg. student@example.com" v-model="student_email">
           </div>
         </div>
         <!-- School Grade -->
         <div class="field">
           <label class="label">School Grade</label>
           <div class="select">
-          <select>
+          <select v-model="student_grade">
             <option value="6">6</option>
             <option value="7">7</option>
             <option value="8">8</option>
@@ -42,28 +47,28 @@
         <div class="field">
           <label class="label">School Name</label>
           <div class="control">
-            <input class="input" type="text" placeholder="eg. Legacy School Bangalore">
+            <input class="input" type="text" placeholder="eg. Legacy School Bangalore" v-model="school_name">
           </div>
         </div>
         <!-- School Email -->
         <div class="field">
           <label class="label">School Email</label>
           <div class="control">
-            <input class="input" type="email" placeholder="eg. school@example.com">
+            <input class="input" type="email" placeholder="eg. school@example.com" v-model="school_email">
           </div>
         </div>
         <!-- addition information -->
         <div class="field">
           <label class="label">Additional information</label>
           <div class="control">
-            <textarea class="textarea is-small" placeholder="Small textarea"></textarea>
+            <textarea class="textarea is-small" placeholder="Small textarea" v-model="add_info"></textarea>
           </div>
         </div>
         <!-- terms and  conditions -->
         <div class="field">
           <div class="control">
             <label class="checkbox">
-              <input type="checkbox">
+              <input type="checkbox" v-model="accepted">
               I agree to the <a href="#">terms and conditions</a>
             </label>
           </div>
@@ -86,6 +91,30 @@
 <script>
 export default {
   name:'Register',
-  props:['event_title']
+  props:['event_title'],
+  data(){
+    return{
+      errors:[],
+      student_name:null,
+      student_email:null,
+      student_grade:null,
+      school_name:null,
+      school_email:null,
+      add_info:null,
+      accepted:false,
+    }
+  },
+  methods:{
+    submit:function(e){
+      if(this.student_name && this.student_email && this.student_grade && this.school_name && this.school_email && this.accepted){
+        console.log("success");
+        console.log(this.student_name)
+        this.$router.push('/about')
+      }
+      else{
+        this.errors.push('Please fill the form completely')
+      }
+    }
+  }
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Congratulations, registration completed successfuly. We look forward to seeing you on the 20th of September 2019</h1>
   </div>
 </template>
