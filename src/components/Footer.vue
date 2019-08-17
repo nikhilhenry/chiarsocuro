faf48a272f1f85e31b75ccf520adee599934b309
Nikhil Henry <nikhilhenry@Nikhils-MBP.Dlink>
2019-08-17 13:44:15 +0530

    create footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,24 @@
   <div>
     <footer class="footer">
       <div class="columns">
-      <div class="column is-two-thirds has-text-left">
       <!-- social media links -->
+      <div class="column is-one-thirds has-text-left">
       <strong class="title is-3">Follow us on social media</strong>
       <div class="container">
       <span class="icon is-large"><a href="https://www.instagram.com/chiarsocuro"><i class="fab fa-instagram fa-2x"></i></a></span>
       <span class="icon is-large"><a href="https://www.instagram.com/chiarsocuro"><i class="fab fa-facebook fa-2x"></i></a></span>
+      </div>
+      <p class="address"><strong class="title is-5">Address: </strong> 6/1A, 6/2 Byrathi Village, Bidarahalli Hobli, Off Hennur-Bagalur Main Road (New, International Airport Link Road), Bengaluru, Karnataka 560077</p>
+      </div>
+      <!-- contact information -->
+      <div class="column has-text-right">
+      <strong class="title is-3 is-spaced">Contact Us</strong>
+      <div class="container">
+        <ul>
+          <li class="subtitle is-5">Events Coordinator: <a href="mailto:events@lsb.edu.in">events@lsb.edu.in</a></li>
+          <li class="subtitle is-5">Student Support: <a href="mailto:chiaroscuro@lsb.edu.in">chiaroscuro@lsb.edu.in</a></li>
+          <li class="subtitle is-5">Phone: <a href="tel:+7022292406">+702292406</a></li>
+        </ul>
       </div>
       </div>
       </div>
@@ -32,5 +44,9 @@ export default {
 <style lang="scss" scoped>
   .footer{
     margin-top:4rem!important;
+    padding-bottom:10px;
+    .address{
+      max-width: 40%;
+    } 
   }
 </style>
