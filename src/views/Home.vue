1189c0fdc42e58b3f9441f5c7ff1fb5289c70c41
Students <students@Students-ka-iMac.local>
2019-08-23 15:37:33 +0530

    add rules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,10 +12,10 @@
   </div>
   <div class="navbar-menu" id="navItems" v-bind:class="{'is-active':isActive}">
     <div class="navbar-end">
-      <a class="navbar-item"><strong>Download Brochure</strong></a>
+      <a class="navbar-item" href="~../assets/brochure.pdf" download><strong>Download Brochure</strong></a>
       <a class="navbar-item" v-scroll-to="'#hero'">Home</a>
       <a class="navbar-item" v-scroll-to="'#events'">Events</a>
-      <a class="navbar-item" v-on="modalToggle">General Guidelines</a>
+      <a class="navbar-item" v-on:click="modalToggle">General Guidelines</a>
       <a class="navbar-item" v-scroll-to="'footer'">Contact Us</a>
     </div>
   </div>
@@ -43,6 +43,18 @@
                <li>Participation is restricted to students of Grade 6-12.</li>
                <li>A student may participate in only one competition.</li>
                <li>All schools must submit their completed registration forms by the deadline - September 5th, 2019.</li>
+               <li>Participants must bring all the required materials as specified for their event.</li>
+               <li>1 faculty chaperone must accompany every 10 participants.</li>
+               <li>Participants will be responsible for all their personal belongings - art supplies, electronics and any other preparatory material. Legacy School Bangalore is not liable for the loss of any items.</li>
+               <li>Plagiarism and other unethical activities will not be tolerated.</li>
+               <li>The use of foul language and any inappropriate behaviour will not be tolerated. In such a situation, the participating team will be disqualified from the competition. </li>
+               <li>The decision of the judges will be final and binding.</li>
+               <li>Additional details / theme for each event will be revealed on the day of the festival. </li>
+               <li>Legacy School Bangalore reserves the right to make any amendments to the rules or events if deemed necessary.</li>
+               <li>Legacy School Bangalore reserves the right to determine / accept participation.</li>
+               <li>Registration fees are non-refundable.</li>
+               <li>All materials given and artworks made are Legacy School’s property. </li>
+               <li>Selected pieces of artwork created during the competition will be showcased with the name of the students / students and their school, at the Sublime Art Gallery UB City after the event. Any sale proceeds will be reimbursed to the student. After the exhibition, remaining art works will be couriered to the respective school at their risk.</li>
              </ul>
            </div>
         </div>
